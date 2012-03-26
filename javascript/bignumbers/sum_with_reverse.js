f8faf44ec76412d839d44fa877ae974339bf849f
Davidson Fellipe <email@fellipe.com>
2012-03-25 23:03:56 -0300

    big numbers sum, with numbers inverting at start

---
+++ javascript/bignumbers/sum_with_reverse.js
@@ -0,0 +1,35 @@
+//to reverse a string
+String.prototype.reverse = function(){
+	"use strict";
+	return this.split("").reverse().join("");
+};
+
+var a = "11111111111111111111111111111111111111111111111111111111111".reverse();
+var b = "11111111111111111111111111111111111111111111111111111111111".reverse();
+
+//get biggest digits amount
+var max = ((a.length > b.length) ? a.length : b.length);
+
+var carry = 0;
+var temp = 0 ;
+
+//final number
+var total = [];
+
+for( var i=0; i<max; i++){
+    
+
+    temp =  parseInt((a[i] ? a[i] : 0), 10) + 
+            parseInt((b[i] ? b[i] : 0), 10) + 
+            carry;
+    
+    total[i] = temp % 10;
+       
+    carry = (temp >= 10) ? 1 : 0;    
+}
+
+if(carry){
+    total[i] = carry;
+}
+
+console.log(total.reverse().join(""));
---
+++ javascript/sort/donkeysort.js
@@ -0,0 +1,51 @@
+Array.prototype.randomize = function() {
+    "use strict";
+
+    if(this.length > 1){
+        for(var i = 0; i < this.length; i++) {
+            this[i] = parseInt(Math.random() * this.length);
+        }
+    }else{
+        this[0] = parseInt(Math.random() * 1000)
+    }
+    return this;
+};
+
+Array.prototype.donkeySort = function(){
+    "use strict";
+    
+    var temp;
+
+    //has swap?
+    var swap = true;
+    
+    while(swap){
+        
+        swap = false;  
+        
+        for(var i=0; i< (this.length - 1); i++){
+
+            if(this[i]>this[i+1]){
+
+                temp = this[i+1];
+                this[i+1] = this[i];
+                this[i] = temp;
+                swap = true;
+            }                    
+        }            
+    }
+    
+    return this;
+};
+
+var v = new Array(1000);
+
+v.randomize();
+
+console.log(v.toString());
+
+console.time("execution time");
+v.donkeySort();
+console.timeEnd("execution time");
+
+console.log(v.toString());
