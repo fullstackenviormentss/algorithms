d6d8bc7366d8d3a2c1b5f2f44b70f0ab811b5cc5
Davidson Fellipe <email@fellipe.com>
2013-12-07 20:11:23 -0200

    code style

--- javascript/bignumbers/sum-with-reverse.js
+++ javascript/bignumbers/sum-with-reverse.js
@@ -1,11 +1,11 @@
 //to reverse a string
 String.prototype.reverse = function(){
-	"use strict";
-	return this.split("").reverse().join("");
+	'use strict';
+	return this.split('').reverse().join('');
 };
 
-var a = "11111111111111111111111111111111111111111111111111111111111".reverse();
-var b = "11111111111111111111111111111111111111111111111111111111111".reverse();
+var a = '11111111111111111111111111111111111111111111111111111111111'.reverse();
+var b = '11111111111111111111111111111111111111111111111111111111111'.reverse();
 
 //get biggest digits amount
 var max = ((a.length > b.length) ? a.length : b.length);
@@ -17,19 +17,18 @@ var temp = 0 ;
 var total = [];
 
 for( var i=0; i<max; i++){
-    
 
-    temp =  parseInt((a[i] ? a[i] : 0), 10) + 
-            parseInt((b[i] ? b[i] : 0), 10) + 
+    temp =  parseInt((a[i] ? a[i] : 0), 10) +
+            parseInt((b[i] ? b[i] : 0), 10) +
             carry;
-    
+
     total[i] = temp % 10;
-       
-    carry = (temp >= 10) ? 1 : 0;    
+
+    carry = (temp >= 10) ? 1 : 0;
 }
 
-if(carry){
+if(carry) {
     total[i] = carry;
 }
 
-console.log(total.reverse().join(""));
+console.log(total.reverse().join(''));
