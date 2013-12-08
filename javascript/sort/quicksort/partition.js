d862e73fe904c4e6dc09fdfe987d20d51649185e
Davidson Fellipe <email@fellipe.com>
2013-12-08 20:28:58 -0200

    removing trailing quicksort

--- javascript/sort/quicksort/partition.js
+++ javascript/sort/quicksort/partition.js
@@ -3,24 +3,25 @@
  *
  */
 Array.prototype.partition = function(){
+
     "use strict";
-    
+
     //pivot is last item
     var pivot = this[this.length-1];
     var cursorSmaller = - 1;
     var swap;
-    
+
     for( var cursorGreater=0; cursorGreater < this.length - 1; cursorGreater++ ){
-            
+
         if( this[ cursorGreater ] < pivot ){
-             
+
            swap = this[ cursorSmaller + 1 ];
 
            this[ cursorSmaller +1 ]  = this[ cursorGreater ];
 
            this[ cursorGreater ] = swap;
-           
-           cursorSmaller++;           
+
+           cursorSmaller++;
         }
     }
 
